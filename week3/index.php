448a5151a773127a7e7e64de05513a1ce0288117
clamy <camm_lamy@hotmail.com>
2012-10-17 14:21:55 -0700

    before lab4

--- week3/index.php
+++ week3/index.php
@@ -17,7 +17,19 @@
 		
 		<table>
 		<!-- This is where we'll put all our content -->
-		
+            <?php
+                include("config.php");
+                $query = "";
+    
+                // Don’t be intimidated by the following lines. You can pretty much
+                // always copy and paste these because they’re sort of like
+                // functional bits that never change.
+                $result = mysql_query($query);
+                while ($row = mysql_fetch_assoc($result)) {
+                    echo $row("title"),"</p>";
+                    echo $row("author"),"</p>";
+                }
+                ?>
 		</table>
 		
 		<script type="text/javascript">
